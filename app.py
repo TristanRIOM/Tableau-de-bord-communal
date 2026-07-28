ce0d20432928b8c0787aa23f1d468ae73c953724
Arbre-Pierre <tristan.riom@gmail.com>
2026-07-28 13:24:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -621,7 +621,7 @@ def afficher_pcaet_nantes(SIREN):
 
 
 SIREN = requests.get(f"https://geo.api.gouv.fr/{'communes' if type_territoire=='Commune' else 'epcis'}/{communes_selectionnees[0][type_territoire=='Commune' and 'code' or 'codeEpci']}?fields=siren").json().get("siren")
-print(SIREN)
+print(f"SIREN de la collectivité séléctionnée : {SIREN}")
 if communes_selectionnees:
     afficher_pcaet_nantes(SIREN)
 
